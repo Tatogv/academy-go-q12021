f7e0d8dca1d6190134b8454955733395508b806d
Fernando <fernando.valadez@wizeline.com>
2021-04-04 22:01:02 -0500

    Added viper!

---
+++ entities/config.go
@@ -0,0 +1,5 @@
+package entities
+
+type Config struct {
+	Port string `mapstructure:"PORT"`
+}
--- main.go
+++ main.go
@@ -2,8 +2,15 @@ package main
 
 import (
 	"api/router"
+	"api/usecase"
+	"log"
 )
 
 func main() {
-	router.CreateRouter()
+	config, err := usecase.LoadConfig()
+	if err != nil {
+		log.Println("Error loading config. Make sure you have an api.env file with the correct configurations.")
+		return
+	}
+	router.CreateRouter(config)
 }
